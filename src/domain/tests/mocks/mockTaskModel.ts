3f915cc8ff4332a27a3c737970abeb52e8852fc4
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-04-22 12:10:57 -0300

    test: create Task model

---
+++ src/domain/tests/mocks/mockTaskModel.ts
@@ -0,0 +1,14 @@
+import { faker } from '@faker-js/faker'
+import { Task } from '../../models';
+
+export const mockTaskModel = (): Task => ({
+  id: faker.datatype.uuid(),
+  title: faker.random.words(5),
+  description: faker.random.words(10),
+  targetDate: faker.date.soon(20),
+  status: 'pending',
+  priority: 'low',
+  project: faker.datatype.uuid(),
+  createdAt: faker.date.recent(20),
+  updatedAt: faker.date.recent(20)
+})
