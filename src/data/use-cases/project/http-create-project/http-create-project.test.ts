f25f84c7fb1afedb94e027b656823b97bb4e99a4
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-05-02 19:45:45 -0300

    test: create HttpCreateProject use case

---
+++ src/data/use-cases/project/http-create-project/http-create-project.test.ts
@@ -0,0 +1,40 @@
+import { UnknownError } from '@domain/errors';
+import { mockProjectModel } from '@domain/test';
+import { HttpEndpoints } from '../../../enums';
+import { HttpMethods } from '../../../protocols';
+import { HttpClientSpy } from '../../../test';
+import { HttpCreateProject } from '.';
+
+const makeSut = () => {
+  const httpClientSpy = new HttpClientSpy();
+  const sut = new HttpCreateProject(httpClientSpy);
+
+  return { sut, httpClientSpy };
+};
+
+describe('Data | Use cases | Project | HttpCreateProject', () => {
+  describe('when creating a project', () => {
+    it('calls the HttpClient with correct data', async () => {
+      const { sut, httpClientSpy } = makeSut();
+      const project = mockProjectModel();
+
+      await sut.create(project);
+
+      expect(httpClientSpy.callsCount).toBe(1);
+      expect(httpClientSpy.url).toBe(HttpEndpoints.CREATE_PROJECT);
+      expect(httpClientSpy.method).toBe(HttpMethods.POST);
+      expect(httpClientSpy.body).toEqual(project);
+    });
+
+    describe('and getting an error', () => {
+      it('throws an UnknownError', async () => {
+        const { sut, httpClientSpy } = makeSut();
+        httpClientSpy.request = jest.fn().mockRejectedValue(new Error());
+
+        await expect(sut.create(mockProjectModel())).rejects.toThrowError(
+          new UnknownError()
+        );
+      });
+    });
+  });
+});
