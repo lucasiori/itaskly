323c2b0ad47203446337b57aa4c46d6fdf8d6952
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-05-11 20:05:22 -0300

    feat: create the ProjectsContextProvider component

---
+++ src/presentation/contexts/projects/projects-provider-types.ts
@@ -0,0 +1,18 @@
+import type { ProjectModel } from '@domain/models';
+import { LoadProject } from '@domain/use-cases';
+import React from 'react';
+
+export type ProjectsContextValue = {
+  data: {
+    projects: ProjectModel[];
+  };
+  state: {
+    loading: boolean;
+    error: string | null;
+  };
+};
+
+export type ProjectsContextProps = {
+  children: React.ReactNode;
+  loadProjects: LoadProject;
+};
---
+++ src/presentation/contexts/projects/projects-provider.tsx
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import type {
+  ProjectsContextProps,
+  ProjectsContextValue,
+} from './projects-provider-types';
+import { ProjectModel } from '@domain/models';
+
+const ProjectsContext = createContext<ProjectsContextValue>(
+  {} as ProjectsContextValue
+);
+
+export const ProjectsContextProvider = ({
+  children,
+  loadProjects,
+}: ProjectsContextProps) => {
+  const [data, setData] = useState<ProjectsContextValue['data']>({
+    projects: [],
+  });
+  const [state, setState] = useState<ProjectsContextValue['state']>({
+    loading: true,
+    error: null,
+  });
+
+  const onLoadSuccess = (response: ProjectModel[]) => {
+    setData({ projects: response });
+    setState({ loading: true, error: null });
+  };
+
+  const onLoadError = () => {
+    setData({ projects: [] });
+    setState({
+      loading: false,
+      error: 'Ocorreu um erro ao buscar os projetos.',
+    });
+  };
+
+  useEffect(() => {
+    loadProjects.loadAll().then(onLoadSuccess).catch(onLoadError);
+  }, [loadProjects]);
+
+  return (
+    <ProjectsContext.Provider value={{ data, state }}>
+      {children}
+    </ProjectsContext.Provider>
+  );
+};
+
+export const useProjectsContext = () => {
+  return useContext(ProjectsContext);
+};
