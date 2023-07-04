76cf13d7c24261d744eb201c006de0cc04094672
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-07-04 12:41:00 -0300

    test: create the status tag component

---
+++ src/presentation/components/status-tag/status-tag.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import { StatusTag } from '.';
+import { theme } from '@presentation/styles';
+
+describe('Presentation | Components | StatusTag', () => {
+  describe('when rendering', () => {
+    describe('and having the pending status', () => {
+      it('renders the component with the correct properties', () => {
+        render(<StatusTag status="pending" />);
+
+        expect(screen.getByText('PENDING')).toBeInTheDocument();
+        expect(screen.getByTestId('status-tag-container')).toHaveStyle({
+          'background-color': theme.colors.purple,
+        });
+      });
+    });
+
+    describe('and having the completed status', () => {
+      it('renders the component with the correct properties', () => {
+        render(<StatusTag status="completed" />);
+
+        expect(screen.getByText('COMPLETED')).toBeInTheDocument();
+        expect(screen.getByTestId('status-tag-container')).toHaveStyle({
+          'background-color': theme.colors.green,
+        });
+      });
+    });
+  });
+});
