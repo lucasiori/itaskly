eceb0186e56796fdd41f4c17a311ea573ccbcfcf
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-07-04 15:54:44 -0300

    feat: redirect to dashboard page when accessing the root

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,7 +1,15 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
 import { Dashboard, NotFound } from '@presentation/pages';
 
 const router = createBrowserRouter([
+  {
+    index: true,
+    element: <Navigate to="/dashboard" />,
+  },
   {
     path: '/dashboard',
     element: <Dashboard />,
