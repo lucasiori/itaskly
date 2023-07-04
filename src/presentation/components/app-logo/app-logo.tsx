92bfbe8be58686ed2374f8d3f97da97ef04dcced
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-07-04 15:35:30 -0300

    feat: update the app logo component width

--- src/presentation/components/app-logo/app-logo.tsx
+++ src/presentation/components/app-logo/app-logo.tsx
@@ -1,5 +1,5 @@
 import image from '@presentation/assets/images/logo.png';
 
 export const AppLogo = () => {
-  return <img src={image} alt="iTaskly" />;
+  return <img src={image} alt="iTaskly" width={100} />;
 };
