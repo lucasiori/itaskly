1655649a772be018d9939fa7a11a60f0708f08d3
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-07-04 16:13:59 -0300

    test: return the error messages from the projects provider

--- src/presentation/contexts/projects/projects-provider.test.tsx
+++ src/presentation/contexts/projects/projects-provider.test.tsx
@@ -6,6 +6,7 @@ import {
   renderHook,
   waitFor,
 } from '@testing-library/react';
+import { faker } from '@faker-js/faker';
 import {
   HttpCreateProjectSpy,
   HttpDeleteProjectSpy,
@@ -23,7 +24,7 @@ type MakeSutProps = {
   renderContextHook?: boolean;
 };
 
-const makeSut = (props: MakeSutProps = {}) => {
+const makeSut = async (props: MakeSutProps = {}) => {
   const httpLoadProjectSpy = new HttpLoadProjectSpy();
   const httpCreateProjectSpy = new HttpCreateProjectSpy();
   const httpUpdateProjectSpy = new HttpUpdateProjectSpy();
@@ -51,6 +52,8 @@ const makeSut = (props: MakeSutProps = {}) => {
         return <ProjectsContextProvider {...useCasesProps} {...props} />;
       },
     });
+
+    await waitFor(() => hook?.result.current);
   }
 
   return {
@@ -65,7 +68,7 @@ const makeSut = (props: MakeSutProps = {}) => {
 describe('Presentation | Contexts | ProjectsContext', () => {
   describe('when rendering the provider', () => {
     it('calls "loadAll" from HttpLoadProject', async () => {
-      const { httpLoadProjectSpy } = makeSut({
+      const { httpLoadProjectSpy } = await makeSut({
         renderContextComponent: true,
       });
 
@@ -76,35 +79,58 @@ describe('Presentation | Contexts | ProjectsContext', () => {
   describe('when rendering the hook', () => {
     describe('and creating a new project', () => {
       it('calls "create" from HttpCreateProject providing the project', async () => {
-        const { hook, httpLoadProjectSpy, httpCreateProjectSpy } = makeSut({
-          renderContextHook: true,
-        });
+        const { hook, httpLoadProjectSpy, httpCreateProjectSpy } =
+          await makeSut({
+            renderContextHook: true,
+          });
         const { projects } = httpLoadProjectSpy;
         const lastProjectId = Number(projects[projects.length - 1].id);
+        const projectTitle = faker.random.words(3);
 
-        await waitFor(() => hook?.result.current);
         await act(async () => {
-          hook?.result.current.handlers.createProject('test');
+          hook?.result.current.handlers.createProject(projectTitle);
         });
 
         expect(httpCreateProjectSpy.project).toEqual({
           id: String(lastProjectId + 1),
-          title: 'test',
+          title: projectTitle,
           status: 'pending',
           createdAt: expect.any(Date),
           updatedAt: expect.any(Date),
+        });
+      });
+
+      describe('and getting an error', () => {
+        it('returns the correct error message', async () => {
+          const { hook, httpCreateProjectSpy } = await makeSut({
+            renderContextHook: true,
+          });
+          httpCreateProjectSpy.create = jest
+            .fn()
+            .mockRejectedValue(new Error());
+
+          await act(async () => {
+            hook?.result.current.handlers.createProject('any_title');
+          });
+
+          expect(hook?.result.current.state).toEqual(
+            expect.objectContaining({
+              error:
+                'Ocorreu um erro ao criar o novo projeto, por favor tente novamente.',
+            })
+          );
         });
       });
     });
 
     describe('and changing the project status', () => {
       it('calls "update" from HttpUpdateProject providing the project with the new status', async () => {
-        const { hook, httpLoadProjectSpy, httpUpdateProjectSpy } = makeSut({
-          renderContextHook: true,
-        });
+        const { hook, httpLoadProjectSpy, httpUpdateProjectSpy } =
+          await makeSut({
+            renderContextHook: true,
+          });
         const projectId = String(httpLoadProjectSpy.projects[0].id);
 
-        await waitFor(() => hook?.result.current);
         await act(async () => {
           hook?.result.current.handlers.changeProjectStatus(
             projectId,
@@ -116,21 +142,107 @@ describe('Presentation | Contexts | ProjectsContext', () => {
           expect.objectContaining({ status: 'completed' })
         );
       });
+
+      describe('and providing an invalid project id', () => {
+        it('returns the correct error message', async () => {
+          const { hook } = await makeSut({ renderContextHook: true });
+
+          await act(async () => {
+            hook?.result.current.handlers.changeProjectStatus(
+              'invalid_id',
+              'pending'
+            );
+          });
+
+          expect(hook?.result.current.state).toEqual(
+            expect.objectContaining({
+              error: 'Projeto não encontrado.',
+            })
+          );
+        });
+      });
+
+      describe('and getting an error', () => {
+        it('returns the correct error message', async () => {
+          const { hook, httpLoadProjectSpy, httpUpdateProjectSpy } =
+            await makeSut({
+              renderContextHook: true,
+            });
+          httpUpdateProjectSpy.update = jest
+            .fn()
+            .mockRejectedValue(new Error());
+          const projectId = String(httpLoadProjectSpy.projects[0].id);
+
+          await act(async () => {
+            hook?.result.current.handlers.changeProjectStatus(
+              projectId,
+              'completed'
+            );
+          });
+
+          expect(hook?.result.current.state).toEqual(
+            expect.objectContaining({
+              error:
+                'Ocorreu um erro ao atualizar o status do projeto, por favor tente novamente.',
+            })
+          );
+        });
+      });
     });
 
     describe('and deleting a project', () => {
       it('calls "delete" from HttpDeleteProject providing the project id', async () => {
-        const { hook, httpLoadProjectSpy, httpDeleteProjectSpy } = makeSut({
-          renderContextHook: true,
-        });
+        const { hook, httpLoadProjectSpy, httpDeleteProjectSpy } =
+          await makeSut({
+            renderContextHook: true,
+          });
         const projectId = String(httpLoadProjectSpy.projects[0].id);
 
-        await waitFor(() => hook?.result.current);
         await act(async () => {
           hook?.result.current.handlers.deleteProject(projectId);
         });
 
         expect(httpDeleteProjectSpy.projectId).toBe(projectId);
+      });
+
+      describe('and providing an invalid project id', () => {
+        it('returns the correct error message', async () => {
+          const { hook } = await makeSut({ renderContextHook: true });
+
+          await act(async () => {
+            hook?.result.current.handlers.deleteProject('invalid_id');
+          });
+
+          expect(hook?.result.current.state).toEqual(
+            expect.objectContaining({
+              error: 'Projeto não encontrado.',
+            })
+          );
+        });
+      });
+
+      describe('and getting an error', () => {
+        it('returns the correct error message', async () => {
+          const { hook, httpLoadProjectSpy, httpDeleteProjectSpy } =
+            await makeSut({
+              renderContextHook: true,
+            });
+          httpDeleteProjectSpy.delete = jest
+            .fn()
+            .mockRejectedValue(new Error());
+          const projectId = String(httpLoadProjectSpy.projects[0].id);
+
+          await act(async () => {
+            hook?.result.current.handlers.deleteProject(projectId);
+          });
+
+          expect(hook?.result.current.state).toEqual(
+            expect.objectContaining({
+              error:
+                'Ocorreu um erro ao excluir o projeto, por favor tente novamente.',
+            })
+          );
+        });
       });
     });
   });
