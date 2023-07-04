4bad249993bc3f9bb9692619d88eeaec648f9193
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-07-04 13:41:50 -0300

    feat: add the add new project component to the projects box component

--- src/presentation/components/projects-box/projects-box.tsx
+++ src/presentation/components/projects-box/projects-box.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
 import { PlusCircle } from '@phosphor-icons/react';
 import { useProjectsContext } from '@presentation/contexts';
 import { AppLogo } from '../app-logo';
 import { IconButton } from '../icon-button';
 import { ProjectsList } from '../projects-list';
+import { AddNewProject } from '../add-new-project';
 import {
   ProjectsBoxContainer,
   ProjectsBoxFooter,
@@ -12,19 +14,34 @@ import {
 export const ProjectsBox = () => {
   const { data, handlers } = useProjectsContext();
 
+  const [isAddingNewProject, setIsAddingNewProject] = useState(false);
+
+  const handleCreateNewProject = (title: string) => {
+    handlers.createProject(title);
+    setIsAddingNewProject(false);
+  };
+
   return (
     <ProjectsBoxContainer>
       <ProjectsBoxHeader>
         <h2>Meus projetos</h2>
+
         <IconButton
           icon={PlusCircle}
           iconProps={{ size: 32, weight: 'regular' }}
+          onClick={() => setIsAddingNewProject(true)}
         />
       </ProjectsBoxHeader>
 
       <ProjectsList
         projects={data.projects}
         onDeleteProject={handlers.deleteProject}
+      />
+
+      <AddNewProject
+        isAdding={isAddingNewProject}
+        onSave={handleCreateNewProject}
+        onCancel={() => setIsAddingNewProject(false)}
       />
 
       <ProjectsBoxFooter>
